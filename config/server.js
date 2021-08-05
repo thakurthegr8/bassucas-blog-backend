c9e85d426a5193f73c3a4ede2bd9e520837e3374
rohan7055 <rohan.thakur7055@gmail.com>
2021-08-05 14:56:38 +0530

    make it better

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b9cc58e45ab9bbd27626b07037aac2f2'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '89bc8f49-1a40-4a39-9eb6-2895cb4cc6c6'
+};
